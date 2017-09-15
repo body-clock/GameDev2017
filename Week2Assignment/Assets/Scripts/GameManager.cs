cbc8314a2b76310f6fcac071a73218a97771bbd8
Unknown <plp248@nyu.edu>
2017-09-15 18:15:35 -0400

    Added turret
    turret rotates towards mouse

--- Week2Assignment/Assets/Scripts/GameManager.cs
+++ Week2Assignment/Assets/Scripts/GameManager.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
 	public int score;
 	public int spawnSpeed;
 	
 	public static GameManager Instance;
 
-	// Use this for initialization
 	void Start ()
 	{
 		Instance = this;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
--- Week2Assignment/Assets/Scripts/Turret.cs
+++ Week2Assignment/Assets/Scripts/Turret.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour {
+public class Turret : MonoBehaviour
+{
+    private void Update()
+    {
+        FaceMouse();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void FaceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y);
+
+        transform.up = direction;
+    }
 }
